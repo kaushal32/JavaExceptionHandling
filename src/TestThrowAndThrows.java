323ff589c5f4ca703b8f76b4c9580fa1fb4d535b
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-11-29 22:08:10 +0530

    Java throw and throws Example

---
+++ src/TestThrowAndThrows.java
@@ -0,0 +1,22 @@
+public class TestThrowAndThrows  
+{  
+    // defining a user-defined method  
+    // which throws ArithmeticException  
+    static void method() throws ArithmeticException  
+    {  
+        System.out.println("Inside the method()");  
+        throw new ArithmeticException("throwing ArithmeticException");  
+    }  
+    //main method  
+    public static void main(String args[])  
+    {  
+        try  
+        {  
+            method();  
+        }  
+        catch(ArithmeticException e)  
+        {  
+            System.out.println("caught in main() method");  
+        }  
+    }  
+}  
