3e7250a4be5c49507509e4aee5eb261d77339c33
Kaushal Kumar <90971842+kaushal32@users.noreply.github.com>
2021-11-29 22:04:30 +0530

    Handle Exception Using try-catch block

---
+++ src/JavaThrows1.java
@@ -0,0 +1,16 @@
+import java.io.*;  
+class M{  
+ void method()throws IOException{  
+  throw new IOException("device error");  
+ }  
+}  
+public class Testthrows2{  
+   public static void main(String args[]){  
+    try{  
+     M m=new M();  
+     m.method();  
+    }catch(Exception e){System.out.println("exception handled");}     
+  
+    System.out.println("normal flow...");  
+  }  
+}  
